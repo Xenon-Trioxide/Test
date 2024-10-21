df626c588c3a13663f8876867813df21d15c15e8
Xenon-Trioxide <141217057+Xenon-Trioxide@users.noreply.github.com>
2024-10-21 23:17:10 +0700

    Update XponentMain.lua

--- XponentMain.lua
+++ XponentMain.lua
@@ -12,7 +12,7 @@ local XPONENT = {
 		USERINPUT_S = game:GetService("UserInputService"),
 		HTTP_S = game:GetService("HttpService"),
 		TP_S = game:GetService("TeleportService"),
-		VIM_S = game:GetService("VirtualInputService")
+		VIM_S = game:GetService("VirtualInputManager")
 	},
 	Modules = {
 		INPUT_M = loadstring(game:HttpGet('https://raw.githubusercontent.com/Xenon-Trioxide/Test/refs/heads/Dev/Modules/Input.lua'))(),
