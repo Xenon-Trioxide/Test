522df8411823614a93e73d173216747f3ced9e64
Xenon-Trioxide <141217057+Xenon-Trioxide@users.noreply.github.com>
2025-01-28 20:45:10 +0700

    Create ANT.lua

---
+++ ANT.lua
@@ -0,0 +1,7 @@
+warn("epic")
+if game.PlaceId == 1213821265 or game.PlaceId == 110057291542440 or game.PlaceId == 121625668096152 or game.PlaceId == 96713840271098 then
+	for i=1,10,1 do
+		game:GetService("TeleportService"):Teleport(1584363130,game.Players.LocalPlayer)
+		wait(.1)
+	end
+end
